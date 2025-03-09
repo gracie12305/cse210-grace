2f7fce07a1f64ee7ba09f9173177d39608b799d6
gracie12305 <kwofib5@gmail.com>
2025-03-09 15:18:16 +0200

    Changed the display massage

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Sandbox Project.");
+        Console.WriteLine("Hello World! This is Amma Bossman.I am a member of the church of Jesus Christ of Latter Day Saint.");
     }
 }
