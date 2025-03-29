b60716ee1b4f74a33585450c2b1b6625815ac2a0
gracie12305 <kwofib5@gmail.com>
2025-03-29 21:40:02 +0200

    CHANGES

--- week03/ScriptureMemorizer/ScriptureMemorizer.cs
+++ week03/ScriptureMemorizer/ScriptureMemorizer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Data;
 using System.Diagnostics.Contracts;
+using System.Security.Cryptography.X509Certificates;
 using System.Transactions;
 
 
@@ -9,25 +10,27 @@ public class Scripture
     private string  _reference;
     
     private List<string>_words = new List<string>();
+    private string IsHidden;
 
 
-    public Scripture(Reference ref, string text)
+    public Scripture(Reference reference, string text)
     {
-        public string Reference;
-         public string _text;
+        _reference = Reference;
+        text = Text;
     }
 
     public void HideRandomWords(int numbersToHide)
     {
+        numbersToHide = IsHidden;
         Random rand = new Random();
         int index;
         do
         {
-            index = rand.Next(Words.Count);
+            index = rand.Next(_words.Count);
         }
-        while (Words[index].IsHidden); // Ensure the word is not already hidden
+        while (_words[index].IsHidden); // Ensure the word is not already hidden
 
-        Words[index].Hide(); // Hide the selected word
+        _words[index].Hide(); // Hide the selected word
     }
 
     public string GetDisplayText()
