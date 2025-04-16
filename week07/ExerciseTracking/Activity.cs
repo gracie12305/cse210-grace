0da3a5ad4915b021d9d730d254672af5d379a8ca
gracie12305 <kwofib5@gmail.com>
2025-04-16 16:59:43 +0200

    week7

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,27 @@
+using System;
+
+
+public abstract class Activity
+{
+    private string _date;
+    private int _minutes;
+
+    public Activity(string  date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public string Date => _date;
+    public int Minutes => _minutes;
+
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public virtual string GetSummary()
+    {
+        return $"{_date} ({_minutes}min):Distance {GetDistance():0.0} km, Speed {GetSpeed():0.0} kph, Pace: {GetPace():0.00} min per km";
+    }
+    
+}
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+
+        
+        Activity run = new Running("23 March 2025", 38, 3.8);
+        Activity cycle = new Cycling("6 April 2025", 100, 12.9);
+        Activity swim = new Swimming("20 April 2025",67, 10);
+        
+        List<Activity> activities = new List<Activity> {run, cycle, swim};
+
+
+        foreach (Activity act in activities)
+        {
+            Console.WriteLine(act.GetSummary());
+        }
     }
+
 }
